fa45e41cb125920adee662859420645b249665c3
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-07-17 17:07:15 -0300

    update

--- Express Server/todolist-v1/app.js
+++ Express Server/todolist-v1/app.js
@@ -5,10 +5,34 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let items = ['Buy Food', 'Cook Food', 'Eat Food '];
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function (req, res) {
-  res.send('hello');
+
+let today = new Date();
+
+let options = {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long'
+};
+
+let day = today.toLocaleDateString('en-US', options);
+
+res.render('list', {kindOfDay: day, newListItems: items});
+
+});
+
+app.post('/', function (req, res) {
+  let item = req.body.newItem;
+
+  items.push(item);
+  res.redirect('/');
+
 });
 
 
