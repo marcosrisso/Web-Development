20f3b5900e5caebd76ea41e73fcb8dcde40f22e0
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-07-18 02:39:28 -0300

    update

--- Express Server/todolist-v1/app.js
+++ Express Server/todolist-v1/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 let items = ['Buy Food', 'Cook Food', 'Eat Food '];
+let workItems = [];
+
 
 app.set('view engine', 'ejs');
 
@@ -24,18 +26,42 @@ let options = {
 
 let day = today.toLocaleDateString('en-US', options);
 
-res.render('list', {kindOfDay: day, newListItems: items});
+res.render('list', {listTitle: day, newListItems: items});
 
 });
 
 app.post('/', function (req, res) {
+
   let item = req.body.newItem;
 
-  items.push(item);
-  res.redirect('/');
+console.log(req.body);
+
+  if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+    res.redirect('/');
+  }
+
 
 });
 
+
+app.get('/work', function (req, res) {
+  res.render('list', {listTitle: 'Work List', newListItems: workItems});
+
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+
+app.post('/work', function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work');
+});
 
 
 app.listen(3000, function () {
