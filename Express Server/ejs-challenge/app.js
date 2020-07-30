19f63bc0b41de34f4a795306dc4431a3cc90e96f
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-07-29 23:33:56 -0300

    update deploy heroku

--- Express Server/ejs-challenge/app.js
+++ Express Server/ejs-challenge/app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-marcos:marshallmathers@cluster0.nzubg.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = {
   title: String,
@@ -75,6 +75,13 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started");
 });
