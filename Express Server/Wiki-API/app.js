6bb2b71856be08c77bef1d083e3e4f7e8871cd3f
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-08-01 01:12:18 -0300

    update

--- Express Server/Wiki-API/app.js
+++ Express Server/Wiki-API/app.js
@@ -13,6 +13,69 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true });
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model('Article', articleSchema);
+
+// Request ALL
+
+app.route('/articles')
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .post(function(req, res) {
+    console.log();
+
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send('Successfully sent');
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send('All articles deleted');
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+  // Request Specifics
+
+  app.route('/articles/:articleTitle')
+
+  .get(function (req, res) {
+    
+  });
+
+// app.get('/articles', );
+//
+// app.post('/articles', );
+//
+// app.delete('/articles', );
+//
+
 app.listen(3000, function() {
   console.log('Server running on Port 3000');
 });
