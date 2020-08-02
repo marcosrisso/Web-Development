6bc8f61df308cb699c24c040372adea3eaf994bd
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-08-02 17:00:31 -0300

    update

--- Express Server/Secrets - Starting Code/app.js
+++ Express Server/Secrets - Starting Code/app.js
@@ -2,12 +2,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/usersDB', {userNewUrlParser: true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model('User', userSchema);
+
 
 app.get('/', function (req, res) {
   res.render('home');
@@ -20,6 +32,46 @@ app.get('/login', function (req, res) {
 app.get('/register', function (req, res) {
   res.render('register');
 });
+
+
+app.post('/register', function (req, res) {
+  const newUser = new User ({
+    email: req.body.username,
+    passwrod: req.body.password
+  });
+});
+
+newUser.save(function (err) {
+  if (err) {
+  console.log(err);
+  } else {
+  res.render('secrets');
+  }
+});
+
+app.post('/login', function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+User.findOne ({
+  email: username, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render('secrets');
+        }
+      }
+    }
+  }
+});
+
+
+
+
+})
+
 
 
 app.listen(3000, function() {
