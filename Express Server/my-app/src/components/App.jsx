672696035d5d63021118474eb6e9f9a43a333e06
Marcos <marcos@Marcoss-MacBook-Pro-3.local>
2020-08-12 02:46:44 -0300

    update keeper app

--- Express Server/my-app/src/components/App.jsx
+++ Express Server/my-app/src/components/App.jsx
@@ -1,16 +1,23 @@
 //jshint esversion: 6
 
-import React from 'react';
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import Note from './Note.jsx';
+  import React from "react";
+  import Header from "./Header";
+  import Footer from "./Footer";
+  import Note from "./Note";
+  import notes from "../notes.js";
 
-function App() {
-  return <div>
-  <Header />
-  <Note />
-  <Footer />
-  </div>
+  function createNotes(note) {
+    return <Note key={note.id} title={note.title} content={note.content} />;
+  }
+
+  function App() {
+    return (
+      <div>
+        <Header />
+        {notes.map(createNotes)}
+        <Footer />
+      </div>
+    );
   }
 
   export default App;
